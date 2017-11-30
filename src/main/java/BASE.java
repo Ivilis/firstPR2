5530a2c1d8b38d54d48803f5cc18d5b915b3be64
Ivilis <holeoffox@gmail.com>
2017-12-01 02:06:49 +0300

    Более мощный калькулятор!

--- src/main/java/BASE.java
+++ src/main/java/BASE.java
@@ -6,18 +6,40 @@ import java.util.Scanner;
 public class BASE {
     public static void main(String[] args) {
 
-
         Scanner ScanThis = new Scanner(System.in);
 
+        //запрашиваем первый операнд
         System.out.println("Print the First Operand");
         float a = ScanThis.nextFloat();
 
+        //запрашиваем второй операнд
         System.out.println("Print the Second Operand");
         float b = ScanThis.nextFloat();
 
+        //запрашиваем операцию
+        System.out.println("Choose your operation from list: + , - , * , /");
+        String z = ScanThis.next();
+
         ScanThis.close();
 
-        float sum = a + b;
-        System.out.printf("Sum of %.4f and %.4f is %.4f", a, b, sum);
+        //выводим выводим результат в зависимости от выбранной операции
+        if (z.equals("+"))
+        {
+            float rez = a + b;
+            System.out.printf("%.4f + %.4f = %.4f", a, b, rez);
+        } else if (z.equals("-"))
+        {
+            float rez = a - b;
+            System.out.printf("%.4f - %.4f = %.4f", a, b, rez);
+        } else if (z.equals("*"))
+        {
+            float rez = a * b;
+            System.out.printf("%.4f * %.4f = %.4f", a, b, rez);
+        } else if (z.equals("/"))
+        {
+            float rez = a / b;
+            System.out.printf("%.4f * %.4f = %.4f", a, b, rez);
+        }
+        else System.out.println("Invalid Operation Symbol!!!");
     }
 }
