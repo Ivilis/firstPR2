7e34dacf8f5cc49d79797e024047d2dd255080e0
Ivilis <holeoffox@gmail.com>
2017-12-01 00:44:05 +0300

    commit first realisation

--- src/main/java/BASE.java
+++ src/main/java/BASE.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
+/**
+ * @author Ivilis
+ */
 public class BASE {
     public static void main(String[] args) {
-        System.out.println("Hello World, baby!");
+
+
+        Scanner ScanThis = new Scanner(System.in);
+
+        System.out.println("Print the First Operand");
+        float a = ScanThis.nextFloat();
+
+        System.out.println("Print the Second Operand");
+        float b = ScanThis.nextFloat();
+
+        ScanThis.close();
+
+        float sum = a + b;
+        System.out.printf("Sum of %.4f and %.4f is %.4f", a, b, sum);
     }
 }
